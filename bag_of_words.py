fcd2251f65fd0cb820cb919a4ef3e85bed881ec8
王沛晟 <115681033@qq.com>
2018-09-11 17:19:21 +0800

    add sklearn and scipy
    add sklearn and scipy

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,6 +1,7 @@
 from read_tsv import get_saved_data
 import pickle
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 
 def generate_dict(train):
     dict = []
@@ -25,15 +26,26 @@ def get_dict():
 def generate_vector(train, dict):
     dict_len = len(dict)
     vector_and_sentiment = []
+    print(dict_len)
+    train = train[:100]
+    word_list = []
     for phrase in train:
-        words = phrase[0].split()
-        vector = [0] * dict_len
-        for word in words:
-            index = dict.index(word)
-            vector[index] = 1
-        vector_and_sentiment.append([vector, phrase[1]])
-    with open("phrase_vector.txt", "wb") as fp:
-        pickle.dump(vector_and_sentiment, fp)
+        words = phrase[0]
+        word_list.append(words)
+    vectorizer = CountVectorizer()
+    print(vectorizer.fit_transform(word_list).todense())
+    print(vectorizer.vocabulary_)
+    print(type(vectorizer.fit_transform(word_list).todense()))
+    # for phrase in train:
+    #     words = phrase[0].split()
+    #     vector = [0] * dict_len
+    #     for word in words:
+    #         index = dict.index(word)
+    #         vector[index] = 1
+    #     vector_and_sentiment.append([vector, phrase[1]])
+    # with open("phrase_vector.txt", "wb") as fp:
+    #     pickle.dump(vector_and_sentiment, fp)
+    # print(vector_and_sentiment)
 
 
 train, test = get_saved_data()
