a00b9d2292ed1ce51a3761ad3eaae311bc87b26a
王沛晟 <115681033@qq.com>
2018-10-23 16:27:01 +0800

    name changes
    name changes

--- read_and_write.py
+++ read_and_write.py
@@ -12,6 +12,23 @@ def load_tsv():
     return data
 
 
+def load_test():
+    data = []
+    with open('test.tsv') as f:
+        reader = csv.reader(f, delimiter='\t')
+        for row in reader:
+            # 'PhraseId', 'SentenceId', 'Phrase'
+            data.append([row[0],row[2]])
+    return data[1:]
+
+
+def write_csv(data, path):
+    writer = csv.writer(open(path, 'w', newline=''))
+    writer.writerow(['PhraseId', 'Sentiment'])
+    for line in data:
+        writer.writerow(line)
+
+
 def split_data(data, ratio):
     phrase_count = len(data)
     train = data[1: int(phrase_count * ratio)]
@@ -35,3 +52,4 @@ def get_saved_data():
 
 # train, test = split_data(load_tsv(), 0.9)
 # save_data(train, test)
+# print(load_test()[:10])
